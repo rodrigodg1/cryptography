d10237b77825e7f520b72d0b04ecbcad6adcf4db
Rodrigo <73913021+rodrigodg1@users.noreply.github.com>
2022-01-02 15:24:41 -0400

    melhorias no código e carregamento de chaves

--- encrypt-decrypt-gui/run.py
+++ encrypt-decrypt-gui/run.py
@@ -7,6 +7,7 @@ from cryptography.hazmat.primitives.asymmetric import utils
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 import os
+import json
 
 
 import tkinter as tk
@@ -16,6 +17,9 @@ from tkinter import messagebox
 import time
 from tkinter import *
 from tkinter import simpledialog
+
+
+
 
 def create_key_pair(passcode="vincere"):
     try:
@@ -49,10 +53,7 @@ def create_key_pair(passcode="vincere"):
         f.write(pem_public_key)
         f.close()
         #lbl_keys_loaded.after(1000, lbl_keys_loaded.destroy)
-        keys_loaded = check_keys()
-        lbl_keys_loaded = tk.Label(fr_buttons,bg="green", text="", textvariable=keys_loaded[0])
-        lbl_keys_loaded.grid(row=7, column=0, sticky="ew", padx=5)
-
+        
         messagebox.showinfo("Chaves", "Par de chaves criado com sucesso ! Salve o em um local seguro !")
         
     except Exception as e:
@@ -76,34 +77,64 @@ def open_file():
     return cipher_text
 
 
+def open_public_key():
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("", "*.pem"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return False
+    with open(filepath, "rb") as key_file:
+        public_key_from_file = serialization.load_pem_public_key(
+            key_file.read()
+        )
+    
+    return public_key_from_file
+
+def open_private_key(passcode):
+    passcode = passcode.encode()
+    """Open a file for editing."""
+    filepath = askopenfilename(
+        filetypes=[("", "*.pem"), ("All Files", "*.*")]
+    )
+    if not filepath:
+        return False
+
+    with open(filepath, "rb") as key_file:
+        private_key_from_file = serialization.load_pem_private_key(
+            key_file.read(),
+            password=passcode,
+        )
+
+    return private_key_from_file
+
+
+
+
+
+
 def encrypt_step_1():
     try:
-        public_key = operations.load_public_key()
-
-        #print(public_key)
+    
+        #public_key = operations.load_public_key()
+        #pega o texto grava em um arquivo e depois le o mesmo arquivo
         text = txt_edit.get(1.0, tk.END)
-        #print(text)
-        
+
+    
         operations.encrypt(text,public_key)
         #print(cypher_text)
         messagebox.showinfo("Criptografia", "Dados criptografados com sucesso!")
         #txt_edit.delete(1.0, tk.END)
         #txt_edit.insert(tk.END, cypher_text)
-
     except Exception as e:
-        print(e)
-        messagebox.showerror("Criptografia", "Erro ao criar o arquivo criptografado!")
-
+        messagebox.showerror("Criptografia", e)
+        #messagebox.showerror("Criptografia", "Falha ao criptografar os dados!")
+    
     
 
 def decrypt_step_1():
-    passcode = simpledialog.askstring("Input", "Informe a senha da chave privada",
-                                parent=window)
-    try:
     
-        private_key = operations.load_private_key(passcode)
-
-
+    try:
         cypher_text = open_file()
     
         plain_text = operations.decrypt(cypher_text,private_key)
@@ -113,30 +144,10 @@ def decrypt_step_1():
         txt_edit.insert(tk.END, plain_text)
         
     except Exception as e:
-        print(e)
-        messagebox.showerror("Descriptografar", "Senha inválida !")
+        messagebox.showerror("Erro",e)
+        #messagebox.showerror("Descriptografar", "Chave Privada Não Carregada !")
 
   
-
-
-def check_keys():
-    keys_loaded = StringVar()
-    try:
-        sk = open("./private_key.pem")
-        pk = open("./public_key.pem")
-        messagebox.showinfo("Chaves", "Chaves carregadas com sucesso!")
-        msg = "Chaves Carregadas"
-        keys_loaded.set('Chaves Carregadas')
-        return keys_loaded,msg
-
-    # Do something with the file
-    except IOError:
-        messagebox.showinfo("Chaves", "Por favor, carregue as chaves para o diretorio atual OU crie um novo par de chaves!")
-        msg = "Chaves Não Carregadas"
-        keys_loaded.set('Chaves Não Carregadas')
-        return keys_loaded,msg
-
-
 
 def save_file(data):
     """Save the current file as a new file."""
@@ -150,6 +161,51 @@ def save_file(data):
         text = txt_edit.get(1.0, tk.END)
         output_file.write(text)
     window.title(f"Text Editor Application - {filepath}")
+
+
+
+def load_public_key_step_1():
+    global public_key
+    
+    #lbl_chave_publica_atual.set("Chave pública não carregada !")
+    try:
+        public_key = open_public_key()
+        if(public_key):
+            messagebox.showinfo("Sucesso", "Chave pública carregada com sucesso !!!")
+            lbl_chave_publica_atual.set("Chave Pública Carregada")
+            color_chave_publica = "green"
+            lbl_chave_publica = tk.Label(fr_buttons,bg=color_chave_publica, textvariable=lbl_chave_publica_atual)
+            lbl_chave_publica.grid(row=11, column=0, sticky="ew", padx=5,pady=5)
+    except Exception as e:
+        messagebox.showerror("Erro", e)
+    
+
+
+def load_private_key_step_1():
+    global private_key
+    
+    
+   # lbl_chave_privada_atual.set("Chave Privada Não Carregada")
+    try:
+        passcode = simpledialog.askstring("Senha", "Informe a senha da chave privada",
+                                    parent=window)
+    except Exception as e:
+        messagebox.showerror("Erro",e)
+
+    try:
+        private_key = open_private_key(passcode)
+        if(private_key):
+            messagebox.showinfo("Sucesso", "Chave privada carregada com sucesso !!!")
+            
+            lbl_chave_privada_atual.set("Chave Privada Carregada")
+            color_chave_privada = "green"
+            lbl_chave_privada = tk.Label(fr_buttons,bg=color_chave_privada, textvariable=lbl_chave_privada_atual)
+            lbl_chave_privada.grid(row=12, column=0, sticky="ew", padx=5,pady=5)
+
+        
+    except Exception as e:
+        messagebox.showerror("Erro",e)
+
 
 
 
@@ -169,9 +225,17 @@ window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
 
-keys_loaded = check_keys()
+lbl_chave_privada_atual = StringVar()
+lbl_chave_publica_atual = StringVar()
 
+lbl_chave_privada_atual.set("Chave privada não carregada")
+lbl_chave_publica_atual.set("Chave pública não carregada")
 
+global color_chave_privada
+global color_chave_publica
+
+color_chave_privada = "red"
+color_chave_publica = "red"
 
 txt_edit = tk.Text(window)
 
@@ -182,21 +246,20 @@ fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=3)
 
 
 btn_encrypt = tk.Button(fr_buttons, text="Criptografar...", command=encrypt_step_1)
-btn_decrypt = tk.Button(fr_buttons, text="Descriptografar...", command=decrypt_step_1)\
+btn_decrypt = tk.Button(fr_buttons, text="Descriptografar...", command=decrypt_step_1)
+btn_load_public_key = tk.Button(fr_buttons, text="Carregar Chave PÚBLICA...", command=load_public_key_step_1)
+btn_load_private_key = tk.Button(fr_buttons, bg="red",text="Carregar Chave PRIVADA...", command=load_private_key_step_1)
 
-if keys_loaded[1] == "Chaves Carregadas":
-    color = "green"
-else:
-    color = "red"
+
+
+
+
 
 btn_create_key_pair = tk.Button(fr_buttons, text="Criar par de chaves ...", command=create_key_pair)
 
-
-
-
-lbl_keys_loaded = tk.Label(fr_buttons,bg=color, text="", textvariable=keys_loaded[0])
 lbl_font_size = tk.Label(fr_buttons, text="Font Size:")
-
+lbl_chave_publica= tk.Label(fr_buttons,bg=color_chave_publica, textvariable=lbl_chave_publica_atual)
+lbl_chave_privada = tk.Label(fr_buttons,bg=color_chave_privada, textvariable=lbl_chave_privada_atual)
 
 button_size_12= Button(fr_buttons, text="12", command= size_12)
 button_size_20= Button(fr_buttons, text="20", command= size_20)
@@ -204,12 +267,17 @@ button_size_20= Button(fr_buttons, text="20", command= size_20)
 #btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 btn_encrypt.grid(row=2, column=0, sticky="ew", padx=5)
 btn_decrypt.grid(row=3, column=0, sticky="ew", padx=5)
-btn_create_key_pair.grid(row=4, column=0, sticky="ew", padx=5)
-lbl_keys_loaded.grid(row=7, column=0, sticky="ew", padx=5)
+btn_load_public_key.grid(row=4, column=0, sticky="ew", padx=5, pady=0)
+btn_load_private_key.grid(row=5, column=0, sticky="ew", padx=5,pady=0)
+btn_create_key_pair.grid(row=6, column=0, sticky="ew", padx=5,pady=0)
+
 
 lbl_font_size.grid(row=8, column=0, sticky="ew", padx=5,pady=15)
 button_size_12.grid(row=9, column=0, sticky="ew", padx=5)
 button_size_20.grid(row=10, column=0, sticky="ew", padx=5)
+
+lbl_chave_publica.grid(row=11, column=0, sticky="ew", padx=5,pady=10)
+lbl_chave_privada.grid(row=12, column=0, sticky="ew", padx=5,pady=0)
 
 fr_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
